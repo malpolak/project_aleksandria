28d6aac56ab28c1ca7e0e4b7a5942d1f7f903d0e
Malwina <malwina.polak@gmail.com>
2023-07-26 20:02:44 +0200

    Registered models in Admin Panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Course, Enrollment, Review
 
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'price', 'start_date', 'end_date']
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Enrollment)
+admin.site.register(Review)
